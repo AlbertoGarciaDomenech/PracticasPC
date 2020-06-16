f638c2ad8e5f93947f08f5817b8b5f25d1696763
Soulsreader <agdbrechas@gmail.com>
2020-06-16 20:22:16 +0200

    P2

--- PracticaPC2/src/PC2/Main.java
+++ PracticaPC2/src/PC2/Main.java
@@ -6,16 +6,18 @@ public class Main {
 			
 		int N = 5;
 		DatosCompartidos dato = new DatosCompartidos(new LockRompeEmpate(2*N), 0);
-		Thread[] hilos = new Thread[2*N];
+		Thread[] hilosInc = new Thread[N];
+		Thread[] hilosDec = new Thread[N];
 
-		for(int i = 0; i < N-1; i++) {
-			hilos[2*i] = new MyThread1(dato, 2*i);
-			hilos[2*i].start();
-			hilos[2*i+1] = new MyThread2(dato, 2*i+1);
-			hilos[2*i+1].start();
+		for(int i = 0; i < N; i++) {
+			hilosInc[i] = new MyThread1(dato, 2*i);
+			hilosInc[i].start();
+			hilosDec[i] = new MyThread2(dato, 2*i+1);
+			hilosDec[i].start();
 		}
 		for(int i = 0; i < N; i++) {
-			hilos[i].join();
+			hilosInc[i].join();
+			hilosDec[i].join();
 		}
 		
 		System.out.println(dato.num);
