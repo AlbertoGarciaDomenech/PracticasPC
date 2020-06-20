f5fd7c0e3399af933e012e78c5498e644bf9c4c1
Soulsreader <agdbrechas@gmail.com>
2020-06-20 19:18:00 +0200

    Finished P4

--- Practica4/src/MultiBufferLockAndCondition/MonitorMultiBuffer2.java
+++ Practica4/src/MultiBufferLockAndCondition/MonitorMultiBuffer2.java
@@ -6,15 +6,15 @@ public class MonitorMultiBuffer2 {
 
 	
 	
-	private int MAX = 15;
+	private int MAX = 10;
 	private volatile ProductoMultiBuffer2[] p = null;
 	private volatile int front, rear, count;
 	private volatile boolean vacio = true, lleno = false;
 	
 	
 	final Lock lock;
-	final Condition colaConsumidores; //cola de espera de hilos consumidores
 	final Condition colaProductores; //cola de espera de hilos productores
+	final Condition colaConsumidores; //cola de espera de hilos consumidores
 	
 	
 	public MonitorMultiBuffer2() {
@@ -22,75 +22,62 @@ public class MonitorMultiBuffer2 {
 		this.p = new ProductoMultiBuffer2[MAX];
 		
 		this.lock = new ReentrantLock();
-		this.colaConsumidores = lock.newCondition();
 		this.colaProductores = lock.newCondition();
+		this.colaConsumidores = lock.newCondition();
 		
 	}
 	
-	public synchronized ProductoMultiBuffer2[] getP(int idCon, int length) {
+	public ProductoMultiBuffer2[] getP(int idCon, int length) {
 		
 		lock.lock();
 
-		ProductoMultiBuffer2 ret[] = new ProductoMultiBuffer2[length];
-		try {
-			
-			while(vacio || length > count)
-				try {
-					colaConsumidores.await();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			System.out.println("Consumidor " + idCon + "consume " + length + ":");
-			for(int i = 0; i < length; i++) {
-				ret[i] = this.p[front];
-				this.p[front] = null; this.p = this.p;
-				System.out.println("producto " + ret[i].id);
-				this.front = (this.front+1) % MAX;
-				this.count--;
-				
+		while(vacio || length > count)
+			try {
+				colaConsumidores.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+		
+		ProductoMultiBuffer2 ret[] = new ProductoMultiBuffer2[length];
+		System.out.println("Consumidor " + idCon + "consume " + length + ":");
+		for(int i = 0; i < length; i++) {
+			ret[i] = this.p[front];
+			this.p[front] = null; this.p = this.p;
+			System.out.println("producto " + ret[i].id);
+			this.front = (this.front+1) % MAX;
+			this.count--;
 			
-			
-			vacio = (count == 0);
-			lleno =  false;
-			
-			colaProductores.signal();
 		}
-		finally {
-			
-			lock.unlock();
-			
-			return ret;
-		}
+		vacio = (count == 0);
+		lleno =  false;
+
+		colaProductores.signal();
+		lock.unlock();
+		return ret;
 	}
 	
-	public synchronized void put(ProductoMultiBuffer2[] pro, int idProd, int length) {
+	public  void put(ProductoMultiBuffer2[] pro, int idProd, int length) {
 		
 		lock.lock();
-		
-		try {
-			
-			while(lleno || length > (MAX - this.count))
-				try {
-					colaProductores.await();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			System.out.println("Productor " + idProd + " produce " + length + ":");
-			for(int i = 0; i < length; i++) {
-				
-				this.p[rear] = pro[i]; this.p = this.p;
-				System.out.println("Producto " + pro[i].id  + " creado");
-				this.rear = (rear+1) % MAX;
-				this.count++;
-				
+		while(lleno || length > (MAX - this.count))
+			try {
+				colaProductores.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-			lleno = (count == MAX);
-			vacio = false;
-			colaConsumidores.signal();
+		System.out.println("Productor " + idProd + " produce " + length + ":");
+		for(int i = 0; i < length; i++) {
 			
-		} finally {			
-			lock.unlock();
+			this.p[rear] = pro[i]; this.p = this.p;
+			System.out.println("Producto " + pro[i].id  + " creado");
+			this.rear = (rear+1) % MAX;
+			this.count++;
+			
 		}
+		lleno = (count == MAX);
+		vacio = false;
+		colaConsumidores.signal();
+		lock.unlock();
+		
 	}
 }
